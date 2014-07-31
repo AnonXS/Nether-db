8d89beb1b2d73e745000ed5ef359894194bc3e05
tyraela <wow@codeart.ch>
2014-07-31 23:03:47 +0200

    Div syntax cleanups. Item/Quest/Spell scripts added

--- clear/1_entity_based/creature.sql
+++ clear/1_entity_based/creature.sql
@@ -13,10 +13,10 @@ DELETE g FROM gameobject AS g
 WHERE g.id NOT IN (SELECT entry FROM gameobject_template)
 OR g.map not in(0, 1, 13, 25, 29, 30, 33, 34, 35, 36, 37, 42, 43, 44, 47, 48, 70, 90, 109, 129, 169, 189, 209, 229, 230, 249, 269, 289, 309, 329, 349, 369, 389, 409, 429, 449, 450, 451, 469, 489, 509, 529, 530, 531, 532, 533, 534, 540, 542, 543, 544, 545, 546, 547, 548, 550, 552, 553, 554, 555, 556, 557, 558, 559, 560, 562, 564, 565, 566, 568, 572, 580, 582, 584, 585, 586, 587, 588, 589, 590, 591, 593, 598);
 
--------------------------------------------------------
+/*-------------------------------------------------------*/
 
 -- clear creature_addon
-DELETE ca FROM creature_addon as ca
+DELETE ca FROM creature_addon AS ca
 WHERE ca.guid NOT IN (SELECT guid FROM creature);
 
 -- clear creature_equipment_template
