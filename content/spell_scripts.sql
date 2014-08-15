33065027963f3a3e779ed736e23aa689c37c3524
tyraela <wow@codeart.ch>
2014-08-15 19:31:58 +0200

    Judgement spellscripts added

---
+++ content/spell_scripts.sql
@@ -0,0 +1,2 @@
+UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `spell_id`='20271' and`ScriptName`='spell_pal_judgement_of_light';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('31898', 'spell_pal_judgement_of_blood');
