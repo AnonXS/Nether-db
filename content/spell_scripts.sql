0b1c9aa6fe6f6399c0962e078cfb7326d0778751
tyraela <wow@codeart.ch>
2014-08-25 13:52:12 +0200

    Touch of Weakness implemented

--- content/spell_scripts.sql
+++ content/spell_scripts.sql
@@ -1,2 +1,7 @@
+/* Paladin */
 UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `spell_id`='20271' and`ScriptName`='spell_pal_judgement_of_light';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('31898', 'spell_pal_judgement_of_blood');
+
+/* Priest */
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('28598', 'spell_pri_touch_of_weakness');
+
